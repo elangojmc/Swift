52c889dbed441fc653bbd35fe148427c0c15480a
perveznouman <PickZyMacMini-6@Ratheeshs-Mac-mini.local>
2018-09-19 12:22:39 +0530

    Chaneges in swift extention file

--- String+Util.swift
+++ String+Util.swift
@@ -14,3 +14,25 @@ extension String {
         return text
     }
 }
+//MARK:- URL to Image
+
+extension UIImageView{
+    
+    func imageforurl(url:String){
+        let get = URL.init(string: url)
+        URLSession.shared.dataTask(with: get!, completionHandler: call).resume()
+        
+    }
+    func call (data:Data?,response:URLResponse?,err:Error?)
+    {
+        if data != nil{
+            
+            DispatchQueue.main.async(execute: {
+                self.image = UIImage.init(data: data!)
+            })
+            
+        }
+        
+    }
+    
+}
