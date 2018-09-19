f2ad7b418837a7d3b0242c532490f7b2719eccab
perveznouman <PickZyMacMini-6@Ratheeshs-Mac-mini.local>
2018-09-19 11:23:58 +0530

    changes done

--- String+Util.swift
+++ String+Util.swift
@@ -1,16 +1,11 @@
 //
 //  String+Util.swift
-//  CoreDataCodable
-//
-//  Created by Andrea Prearo on 3/31/18.
-//  Copyright © 2018 Andrea Prearo. All rights reserved.
-//
 
 import Foundation
 
 extension String {
     static func emptyIfNil(_ optionalString: String?) -> String {
-        let text: String
+    let text: String
         if let unwrapped = optionalString {
             text = unwrapped
         } else {
